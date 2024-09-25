57b847f4727edd189af5825058c3974df2ee009b
DRSRDMR <ahirwaaa@gmail.com>
2024-09-25 12:58:45 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,6 +1,19 @@
 const decodeTheRing = function (s, p) {
 
-    // write your code here
+    // write your code function decodeTheRing(str, pattern) {
+    if (str.length !== pattern.length) return false;
+    
+    for (let i = 0; i < str.length; i++) {
+        if (pattern[i] !== '*' && pattern[i] !== str[i]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+module.exports = decodeTheRing;
+
 
   };
   
